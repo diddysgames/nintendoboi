aa1b5d4a37c11fb1bc09aec68f487a32251ba041
Do_Not_Dusturb <152636544+Do-Not-Dusturb@users.noreply.github.com>
2025-02-12 21:03:17 -0500

    Update custom-cursor.js

--- JS/custom-cursor.js
+++ JS/custom-cursor.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const img = document.createElement("img");
-    img.src = "https://iili.io/2m85zq7.webp"; 
+    img.src = "https://i.postimg.cc/0N4wBx90/Bloxy-Icon.webp";
     img.style.position = "absolute";
-    img.style.pointerEvents = "none"; 
-    img.style.transform = "translate(-50%, -50%)"; 
-    img.style.width = "32px"; 
-    img.style.height = "32px"; 
+    img.style.pointerEvents = "none";
+    img.style.transform = "translate(-50%, -50%)";
+    img.style.width = "32px";
+    img.style.height = "32px";
     img.style.transition = "transform 0.1s linear";
     document.body.appendChild(img);
 
-    // Hi guys
     document.body.style.cursor = "none";
 
     document.addEventListener("mousemove", (e) => {
@@ -21,13 +20,22 @@ document.addEventListener("DOMContentLoaded", function () {
         element.addEventListener("mouseenter", () => {
             let angle = 0;
             img.rotationInterval = setInterval(() => {
-                angle += 20;
+                angle += 10;
                 img.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
             }, 50);
         });
         element.addEventListener("mouseleave", () => {
             clearInterval(img.rotationInterval);
             img.style.transform = "translate(-50%, -50%)";
+        });
+    });
+
+    document.querySelectorAll("iframe").forEach(iframe => {
+        iframe.addEventListener("mouseenter", () => {
+            img.style.display = "none";
+        });
+        iframe.addEventListener("mouseleave", () => {
+            img.style.display = "block";
         });
     });
 });
